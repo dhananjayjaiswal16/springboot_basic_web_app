9c2fed0c64521728435173f13e677d5533cfbb5f
Dhananjay <dhananjayman11@gmail.com>
2022-05-19 11:20:18 +0530

    basic /home

---
+++ src/test/java/myWebApp/demo/SpringWebAppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package myWebApp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebAppDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
